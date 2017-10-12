b813a90d24820df03f7fe44cb48d46a2fc9fad27
jamespeterson <jamespeterson0918@yahoo.com>
2017-10-12 14:29:29 -0400

    made routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const UsersController = require('./routes/UsersController')
 mongoose.Promise = global.Promise
 
 const app = express()
@@ -19,6 +20,8 @@ connection.on('error', (err) => {
 
 app.use(express.static(`${__dirname}/client/build`))
 app.use(bodyParser.json())
+
+app.use('/api/users', UsersController)
 
 app.get('/', (req, res) => {
         res.sendFile(`${__dirname}/client/build/index.html`)
