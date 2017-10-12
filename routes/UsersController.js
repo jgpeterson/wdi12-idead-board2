1cf0fee08aac34a14d860c0f1b5febb5bd750b2d
jamespeterson <jamespeterson0918@yahoo.com>
2017-10-12 16:22:33 -0400

    did handle change

--- routes/UsersController.js
+++ routes/UsersController.js
@@ -12,5 +12,14 @@ catch (err) {
 }
 })
 
+router.post('/', async (req, res) => {
+    try {
+    const newUser = new User(req.body.user)
+    const saved = await newUser.save()
+    res.json(saved)
+    } catch (err) {
+        res.send(err)
+    }
+})
 
 module.exports = router
