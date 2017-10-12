2306bb9e8b4798737171f9110895885fa9d576f7
jamespeterson <jamespeterson0918@yahoo.com>
2017-10-12 10:59:11 -0400

    set up stuff

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+mongoose.Promise = global.Promise
+
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log('Successfully connected to MongoDB')
+})
+
+conection.on('error', (err) => {
+    console.log('MongoDB Error: ', err)
+})
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log("APP listening on port: ", PORT)
+})
